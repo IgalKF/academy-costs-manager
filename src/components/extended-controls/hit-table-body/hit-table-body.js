31b0610338ed0278308920077486a0c80565f765
tamaraSlu <tamaramami@gmail.com>
2023-09-19 19:48:34 +0300

    Divided table to sub components

---
+++ src/components/extended-controls/hit-table-body/hit-table-body.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import HitTableRow from "../../extended-controls/hit-table-row/hit-table-row";
+
+const HitTableBody = (props) => {
+  const { visibleRows, emptyRows,isSelected } = props;
+  return (
+    <TableBody>
+      {visibleRows.map((row, index) => {
+        const isItemSelected = isSelected(row.name);
+        const labelId = `enhanced-table-${index}`;
+
+        return (
+          <HitTableRow
+            rowData={row}
+            labelId={labelId}
+            isItemSelected={isItemSelected}
+          />
+        );
+      })}
+      {emptyRows > 0 && (
+        <TableRow
+          style={{
+            height: 53 * emptyRows,
+          }}
+        >
+          <TableCell colSpan={6} />
+        </TableRow>
+      )}
+    </TableBody>
+  );
+};
+
+export default HitTableBody;
--- src/components/extended-controls/hit-table-row/hit-table-row.js
+++ src/components/extended-controls/hit-table-row/hit-table-row.js
@@ -1,7 +1,25 @@
 import React from "react";
-
-const HitTableRow = () => {
-    return <div className='hit-table-row'></div>
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+const HitTableRow = (props) => {
+    const {rowData,labelId,isItemSelected}=props;
+    return <TableRow
+    key={rowData.name}
+    selected={isItemSelected}
+  >
+    <TableCell
+      component="th"
+      id={labelId}
+      scope="row"
+      padding="none"
+    >
+      {rowData.name}
+    </TableCell>
+    <TableCell align="right">{rowData.calories}</TableCell>
+    <TableCell align="right">{rowData.fat}</TableCell>
+    <TableCell align="right">{rowData.carbs}</TableCell>
+    <TableCell align="right">{rowData.protein}</TableCell>
+  </TableRow>
 };
 
-export { HitTableRow };
+export default HitTableRow;
