2caf6056d4ded11ebf579d7d67ce3cccee0989b7
igal2010 <igalkf@gmail.com>
2023-09-20 22:29:25 +0300

    Filter initial implementation

--- src/components/extended-controls/hit-filter/hit-filter.js
+++ src/components/extended-controls/hit-filter/hit-filter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { InvalidPropertyException } from "../../../domain-model/exceptions/invalid-property-exception";
+import { HitForm } from "../hit-form/hit-form";
 
 const HitFilter = ({ filters }) => {
     if (!Array.isArray(filters)) {
@@ -7,8 +8,20 @@ const HitFilter = ({ filters }) => {
         throw new InvalidPropertyException('HitFilter', 'filters', 'Should be an array.');
     }
 
+    const filterFormOptions = [
+        {
+            type: 'date',
+        },
+        {
+            type: 'select',
+        }
+    ];
+
     const filterControls = filters.map()
     return (<div className='hit-filter'>
+        <HitForm>
+            
+        </HitForm>
     </div>);
 };
 
--- src/components/extended-controls/hit-table-row/hit-table-row.js
+++ src/components/extended-controls/hit-table-row/hit-table-row.js
@@ -2,8 +2,8 @@ import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 const HitTableRow = (props) => {
-    const {rowData,labelId,isItemSelected}=props;
-    return <TableRow
+  const { rowData, labelId, isItemSelected } = props;
+  return <TableRow
     key={rowData.name}
     selected={isItemSelected}
   >
